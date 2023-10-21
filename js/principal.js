967f8582a35d95c599949c8aaa825bafa65f6c1c
JJOrtizChaverra <juanjochaverra02@gmail.com>
2023-10-21 15:07:08 -0500

    Creando una funcion para mostrar y otra para ocultar la ventana modal de crear tienda

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,30 @@
 const ventanaCrearTienda = document.getElementById('ventana-crear-tienda');
 const efectoBlur = document.getElementById('efecto-blur');
 const body = document.getElementsByTagName('body')[0];
+const btnCrearTienda = document.getElementById('btn-crear-tienda');
 
+const btnCancelarCrearTienda = document.getElementById('btn-cancelar-crear-tienda');
+const btnConfirmarCrearTienda = document.getElementById('btn-confirmar-crear-tienda');
+
+// Funcion para mostrar la ventana modal de crear tienda
+
+function mostrarVentanaCrearTienda() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    body.style.overflow = 'hidden'
+    efectoBlur.style.display = 'block';
+    ventanaCrearTienda.style.display = 'block';
+}
+
+btnCrearTienda.addEventListener('click', mostrarVentanaCrearTienda);
+
+function ocultarVentanaCrearTienda() {
+    body.style.overflow = 'auto';
+    efectoBlur.style.display = 'none';
+    ventanaCrearTienda.style.display = 'none';
+}
+
+btnCancelarCrearTienda.addEventListener('click', ocultarVentanaCrearTienda);
+btnConfirmarCrearTienda.addEventListener('click', ocultarVentanaCrearTienda);
