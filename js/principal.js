a4ef27fcc1ee6c1157a9ef3347812e1a5fb08961
JJOrtizChaverra <juanjochaverra02@gmail.com>
2023-10-21 18:11:40 -0500

    Aplicando la interaccion y la validacion del formulario al modal de añadir productos

--- js/principal.js
+++ js/principal.js
@@ -1,14 +1,21 @@
 const ventanaCrearTienda = document.getElementById('ventana-crear-tienda');
 const ventanaEliminarTienda = document.getElementById('ventana-eliminar-tienda');
+const ventanaAñadirProducto = document.getElementById('ventana-añadir-producto');
 
 const btnCrearTienda = document.getElementById('btn-crear-tienda');
 const btnEliminarTienda = document.getElementById('btn-eliminar-tienda');
+const btnAñadirProducto = document.getElementById('btn-añadir-producto');
 
 const btnCancelarCrearTienda = document.getElementById('btn-cancelar-crear-tienda');
 const btnConfirmarCrearTienda = document.getElementById('btn-confirmar-crear-tienda');
 const inputNombreTienda = document.getElementById('name-store');
 
 const btnCancelarEliminarTienda = document.getElementById('btn-cancelar-eliminar-tienda');
+
+const btnCancelarAñadirProducto = document.getElementById('btn-cancelar-añadir-producto');
+const inputNombreProducto = document.getElementById('product-name');
+const inputPrecioProducto = document.getElementById('product-price');
+const inputExistenciasProducto = document.getElementById('product-exists');
 
 const efectoBlur = document.getElementById('efecto-blur');
 const body = document.getElementsByTagName('body')[0];
@@ -26,6 +33,7 @@ const mostrarVentanaModal = ventana => {
     // Mostrar ventanas
     ventana.style.display = 'flex';
 }
+
 const ocultarVentanasModales = ventana => {
     body.style.overflow = 'auto';
     efectoBlur.style.display = 'none';
@@ -49,10 +57,26 @@ const validarCrearTienda = () => {
     }
 }
 
+const validarAñadirProducto = () => {
+    const validarNombreProducto = inputNombreProducto.value.trim();
+    const validarPrecioProducto = inputPrecioProducto.value.trim();
+    const validarExistenciasProducto = inputExistenciasProducto.value.trim();
+
+    if (validarNombreProducto === "" || validarPrecioProducto === "" || validarExistenciasProducto === "") {
+        alert("Debes llenar todos los campos");
+        return false;
+    } else {
+        alert("Producto añadido a la tienda!")
+        return true;
+    }
+}
+
 // Eventos de los botones para mostrar sus respectivas ventanas
 
 btnCrearTienda.addEventListener('click', function () { mostrarVentanaModal(ventanaCrearTienda) });
 btnEliminarTienda.addEventListener('click', function () { mostrarVentanaModal(ventanaEliminarTienda) });
+btnAñadirProducto.addEventListener('click', function () { mostrarVentanaModal(ventanaAñadirProducto) })
 
 btnCancelarCrearTienda.addEventListener('click', function () { ocultarVentanasModales(ventanaCrearTienda) });
 btnCancelarEliminarTienda.addEventListener('click', function () { ocultarVentanasModales(ventanaEliminarTienda) });
+btnCancelarAñadirProducto.addEventListener('click', function () { ocultarVentanasModales(ventanaAñadirProducto) })
