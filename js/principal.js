00b1babc079aa2b4193d7a5a095be28f65d62a19
JJOrtizChaverra <juanjochaverra02@gmail.com>
2023-10-21 16:20:42 -0500

    Organizacion y optimizacion del codigo javascript y interaccion con la ventana modal de eliminar tienda

--- js/principal.js
+++ js/principal.js
@@ -1,39 +1,42 @@
 const ventanaCrearTienda = document.getElementById('ventana-crear-tienda');
-const efectoBlur = document.getElementById('efecto-blur');
-const body = document.getElementsByTagName('body')[0];
+const ventanaEliminarTienda = document.getElementById('ventana-eliminar-tienda');
+
 const btnCrearTienda = document.getElementById('btn-crear-tienda');
+const btnEliminarTienda = document.getElementById('btn-eliminar-tienda');
 
 const btnCancelarCrearTienda = document.getElementById('btn-cancelar-crear-tienda');
 const btnConfirmarCrearTienda = document.getElementById('btn-confirmar-crear-tienda');
-
 const inputNombreTienda = document.getElementById('name-store');
 
-// Funcion para mostrar la ventana modal de crear tienda
+const btnCancelarEliminarTienda = document.getElementById('btn-cancelar-eliminar-tienda');
 
-function mostrarVentanaCrearTienda() {
+const efectoBlur = document.getElementById('efecto-blur');
+const body = document.getElementsByTagName('body')[0];
+
+// Funcion para mostrar la ventana modal de crear tienda
+const mostrarVentanaModal = ventana => {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
-    body.style.overflow = 'hidden'
+    body.style.overflow = 'hidden';
     efectoBlur.style.display = 'block';
-    ventanaCrearTienda.style.display = 'flex';
     inputNombreTienda.focus();
+
+    // Mostrar ventanas
+    ventana.style.display = 'flex';
 }
-
-btnCrearTienda.addEventListener('click', mostrarVentanaCrearTienda);
-
-function ocultarVentanaCrearTienda() {
+const ocultarVentanasModales = ventana => {
     body.style.overflow = 'auto';
     efectoBlur.style.display = 'none';
-    ventanaCrearTienda.style.display = 'none';
-}
 
-btnCancelarCrearTienda.addEventListener('click', ocultarVentanaCrearTienda);
+    // Ocultar ventanas
+    ventana.style.display = 'none';
+}
 
 // Funcion para validar que el usuario no cree una tienda con el input vacio
 
-function validarCrearTienda() {
+const validarCrearTienda = () => {
     const validarInput = inputNombreTienda.value.trim();
 
     if (validarInput === "") {
@@ -45,3 +48,11 @@ function validarCrearTienda() {
         return true;
     }
 }
+
+// Eventos de los botones para mostrar sus respectivas ventanas
+
+btnCrearTienda.addEventListener('click', function () { mostrarVentanaModal(ventanaCrearTienda) });
+btnEliminarTienda.addEventListener('click', function () { mostrarVentanaModal(ventanaEliminarTienda) });
+
+btnCancelarCrearTienda.addEventListener('click', function () { ocultarVentanasModales(ventanaCrearTienda) });
+btnCancelarEliminarTienda.addEventListener('click', function () { ocultarVentanasModales(ventanaEliminarTienda) });
