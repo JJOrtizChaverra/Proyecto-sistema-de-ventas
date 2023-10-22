c88c802d71042e8e45b6ab8db74fb34d9fffb555
JJOrtizChaverra <juanjochaverra02@gmail.com>
2023-10-21 20:25:09 -0500

    Recepcionando los datos de registro del usuario e insertandolo en la base de datos

--- php/registro.php
+++ php/registro.php
@@ -1,1 +1,21 @@
-<
+<?php
+
+include('./conexion.php');
+
+if ($_POST) {
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $correo = $_POST['correo'];
+    $cedula = (int) $_POST['cedula'];
+    $fecha_nacimiento = $_POST['nacimiento'];
+    $contrasenia = md5($_POST['contraseña']);
+
+    $con = conexion();
+    $insert = "INSERT INTO usuarios VALUES($cedula, '$nombre', '$apellido', '$correo', '$fecha_nacimiento', '$contrasenia')";
+
+    $exec_query = mysqli_query($con, $insert);
+
+    header("Location: ../principal.html");
+}
+
+?>
