59f1a882a1817f5d9c71937b89c7ddf4b06a6540
JJOrtizChaverra <juanjochaverra02@gmail.com>
2023-10-21 20:12:40 -0500

    Conectandome a la base de datos local

--- php/conexion.php
+++ php/conexion.php
@@ -1,14 +1,17 @@
 <?php
 
 function conexion() {
+    // Variables de sesion
     $host = "localhost";
     $user = "root";
     $pass = "";
     $db = "sistema_ventas";
 
+    // Conectandonos a la base de datos
     $connect = mysqli_connect($host, $user, $pass);
     mysqli_select_db($connect, $db);
 
+    // Retornando la connexion
     return $connect;
 }
 
